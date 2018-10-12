7af4e180dee32cbb8f6bb6be345041b5b30f9a27
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-12 15:18:10 +0300

    Added events, users_events tables.

---
+++ db/migrate/20181012121426_create_users_events.rb
@@ -0,0 +1,8 @@
+class CreateUsersEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users_events do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :event, index: true
+    end
+  end
+end
