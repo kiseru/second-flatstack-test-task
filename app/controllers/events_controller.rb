b9466a1d684d918c266185df84d63b3fadf1d2b0
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-13 23:46:02 +0300

    Secured actions edit, show, destroy and edit page.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,10 @@
 class EventsController < ApplicationController
+  before_action :require_login, only: %i[edit update destroy]
+
   def index
     if params[:my] == 'true'
       return redirect_to new_user_session_path unless user_signed_in?
+
       @events = current_user.events
     else
       @events = Event.all
@@ -10,6 +13,7 @@ class EventsController < ApplicationController
 
   def new
     return redirect_to new_user_session_path unless user_signed_in?
+
     @event = Event.new
   end
 
@@ -38,5 +42,13 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @event.destroy
     redirect_to events_path
+  end
+
+  private
+
+  def require_login
+    return redirect_to new_user_session unless user_signed_in?
+
+    redirect_to events_path unless current_user == Event.find(params[:id]).user
   end
 end
