a26c5c04d887eee211feeb99896f3553322bcad8
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-13 23:23:28 +0300

    Added removing events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,4 +33,10 @@ class EventsController < ApplicationController
   def edit
     @event = Event.find(params[:id])
   end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
 end
