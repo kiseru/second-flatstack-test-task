874ff76c2040beb39ddb9675543dd4bff4a2b527
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-13 18:40:45 +0300

    Added "Sign In" and "Sign Out" buttons into header.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'auth'
 
-  get 'profile', to: 'profile#index', as: 'user_root'
+  get 'profile', to: 'profile#index'
 
   get 'events', to: 'events#index'
   post 'events', to: 'events#create'
