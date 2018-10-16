92ef6e37ed10d14a1689427741fa0e264efdb94b
Alexandr Kiselev <kis.alex97@gmail.com>
2018-10-16 20:11:49 +0300

    Added config.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module SecondFlatstackTestTask
     config.load_defaults 5.2
     config.i18n.default_locale = :ru
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
